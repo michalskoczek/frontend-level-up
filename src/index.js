12a9ca3e4928a308997294fb65313e1bcd4048c0
Michał Skoczek <mwskoczek@gmail.com>
2020-04-28 22:46:43 +0200

    Add scrollToTop script

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './styles/style.scss';
 import allProducts from './scripts/allProducts';
 import getCurrentYear from './scripts/currentYear';
+import scrollToTop from './scripts/scrollToTop';
 
 window.addEventListener('DOMContentLoaded', (e) => {
   allProducts();
   getCurrentYear();
+  scrollToTop();
 });
---
+++ src/scripts/scrollToTop.js
@@ -0,0 +1,20 @@
+export default function () {
+  const arrowBtn = document.querySelector('.arrow-to-top');
+
+  document.addEventListener('scroll', () => {
+    if (window.scrollY >= screen.availHeight) {
+      arrowBtn.style.display = 'block';
+    } else {
+      arrowBtn.style.display = 'none';
+    }
+  });
+
+  arrowBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+}
