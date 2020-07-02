cbf995bc2cf8674e4d111174db76811613f61b41
Michał Skoczek <mwskoczek@gmail.com>
2020-07-02 13:22:18 +0200

    Added new feature - download after clicking allProducts

--- src/scripts/allProducts.js
+++ src/scripts/allProducts.js
@@ -1,23 +1,35 @@
-export default function (productsArray) {
-  const productsContainer = document.querySelector('.products-container');
-  const btnAdd = document.querySelector('.products__btn-all-products');
+import getData from "./getData";
+import showError from './error';
 
-  btnAdd.addEventListener('click', (e) => {
+const productsContainer = document.querySelector('.products-container');
+const btnAdd = document.querySelector('.products__btn-all-products');
+
+const getAllProducts = () => {
+  btnAdd.addEventListener('click', e => {
     e.preventDefault();
 
-    for (let i = 0; i < productsArray.length; i++) {
-      const productCart = document.createElement('div');
-      productCart.classList.add('col-6');
-      productCart.classList.add('col-lg-4');
-      productCart.classList.add('col-xl-3');
+    getData()
+      .then(resp => {
+        showAllProducts(resp.slice(0, 8));
+      })
+      .catch(() => showError())
+  })
+}
 
-      productCart.innerHTML = `<div class="product-cart products__product-cart">
+function showAllProducts(productsArray) {
+  for (let i = 0; i < productsArray.length; i++) {
+    const productCart = document.createElement('div');
+    productCart.classList.add('col-6');
+    productCart.classList.add('col-lg-4');
+    productCart.classList.add('col-xl-3');
+
+    productCart.innerHTML = `<div class="product-cart products__product-cart">
               <div class="product-cart__img">
               <img src="http://${productsArray[i].imageUrl}">
               </div>
               <p class="product-cart__special-offer">
                 <span>
-                  <img src="./assets/kz-bolt-line.png" alt="" />
+                  <img src="" alt="" />
                 </span>
                 NEW
               </p>
@@ -25,13 +37,13 @@ export default function (productsArray) {
               <p class="product-cart__price">${productsArray[i].price}</p>
               <button class="product-cart__btn">
                 <span>Select option</span>
-                <img src="./assets/kz-heart.png" alt="" />
+                <img src="" alt="" />
               </button>
             </div>`;
 
-      productsContainer.appendChild(productCart);
-    }
-
-    btnAdd.style.display = 'none';
-  });
+    productsContainer.appendChild(productCart);
+  }
+  btnAdd.style.display = 'none';
 }
+
+export default getAllProducts;
