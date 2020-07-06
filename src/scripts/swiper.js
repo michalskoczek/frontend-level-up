0e0bb3194881138680ebc34c1901dbbfecedbe6b
Michał Skoczek <mwskoczek@gmail.com>
2020-07-06 20:54:51 +0200

    Added new feature - added slider by JS

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,19 +1,33 @@
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: false,
-  spaceBetween: 10,
-  slidesPerView: 3,
+import leftArrowBtn from '../assets/left-arrow.png';
+import rightArrowBtn from '../assets/right-arrow.png';
 
+export default function (productsArray) {
+  const btnArrow = [...document.querySelectorAll('.slider__arrow')];
+  const leftArrowImg = document.createElement('img');
+  leftArrowImg.src = `${leftArrowBtn}`;
+  const rightArrowImg = document.createElement('img');
+  rightArrowImg.src = `${rightArrowBtn}`;
+  btnArrow[0].appendChild(leftArrowImg);
+  btnArrow[1].appendChild(rightArrowImg);
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  const imagesRow = document.querySelector('.images__row');
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-})
+  productsArray.forEach(product => {
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img');
+    let imgContent = document.createElement('img');
+    imgContent.classList.add('img__content');
+    imgContent.src = `http://${product.imageUrl}`;
+    let spanName = document.createElement('span');
+    spanName.classList.add('img__name');
+    spanName.textContent = `${product.name}`;
+    let spanPrice = document.createElement('span');
+    spanPrice.classList.add('img__price');
+    spanPrice.textContent = `${product.price}`;
+
+    imgContainer.appendChild(imgContent);
+    imgContainer.appendChild(spanName);
+    imgContainer.appendChild(spanPrice);
+    imagesRow.appendChild(imgContainer);
+  })
+}
